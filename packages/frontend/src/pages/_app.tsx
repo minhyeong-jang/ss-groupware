18cf03fb57659ab374b0c3624817ac60f7ca7b23
Minhyeong Jang <public.doriri@gmail.com>
2022-01-13 18:11:14 +0900

    feat: 아이폰 input 자동확대 막기

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -11,6 +11,10 @@ const App = ({ Component }: AppProps) => {
       <GlobalStyle />
       <Head>
         <title>무신사 - 스타일쉐어</title>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+        />
       </Head>
       <Component />
     </ThemeProvider>
