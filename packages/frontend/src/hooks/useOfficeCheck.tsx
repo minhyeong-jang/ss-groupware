d46d71d4c4dd0daf61ee38dfa35057ef791ecf08
Minhyeong Jang <public.doriri@gmail.com>
2022-01-14 22:36:35 +0900

    feat: 코드 커밋 누락

--- packages/frontend/src/hooks/useOfficeCheck.tsx
+++ packages/frontend/src/hooks/useOfficeCheck.tsx
@@ -5,36 +5,45 @@ import {
   postCheckout,
   PostCheckoutParams,
 } from "apis";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { ErrorModel } from "schema";
 
 export const useOfficeCheck = () => {
+  const [loading, setLoading] = useState(false);
+
   const onCheckin = useCallback(
     async (params: PostCheckinParams) => {
+      setLoading(true);
       try {
         const res = await postCheckin(params);
         message.info(res.data.message);
         return res.data.message;
       } catch (e) {
         message.error((e as ErrorModel).response?.data?.message);
+      } finally {
+        setLoading(false);
       }
     },
     [postCheckin]
   );
   const onCheckout = useCallback(
     async (params: PostCheckoutParams) => {
+      setLoading(true);
       try {
         const res = await postCheckout(params);
         message.info(res.data.message);
         return res.data.message;
       } catch (e) {
         message.error((e as ErrorModel).response?.data?.message);
+      } finally {
+        setLoading(false);
       }
     },
     [postCheckout]
   );
 
   return {
+    loading,
     onCheckin,
     onCheckout,
   };
