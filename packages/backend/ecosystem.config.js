5411dead56fcc02904442d14f54181091d50c93d
Minhyeong Jang <public.doriri@gmail.com>
2022-05-08 20:55:15 +0900

    feat: ecosystem config 변경

--- packages/backend/ecosystem.config.js
+++ packages/backend/ecosystem.config.js
@@ -2,13 +2,12 @@ module.exports = {
   apps: [
     {
       name: "BE - SS - MUSINSA", // 애플리케이션 이름
-      script: "yarn build && build/index.js", // PM2를 실행시켜줄 스크립트 파일의 경로
-      instances: 2, // 설치될 앱 인스턴스가 갯수
+      script: "yarn && yarn run build && build/index.js", // PM2를 실행시켜줄 스크립트 파일의 경로
+      instances: 1, // 설치될 앱 인스턴스가 갯수
       autorestart: true, // 앱 크러쉬 등으로 인해 앱이 종료될 때 재시작할지 여부 (default: true)
-      watch: false, // 폴더 내의 파일에 변경이 있을때, 앱이 리로딩 여부
+      watch: true, // 폴더 내의 파일에 변경이 있을때, 앱이 리로딩 여부
       max_memory_restart: "256M",
       env: {
-        // 앱의 env를 설정
         NODE_ENV: "development",
       },
       env_production: {
