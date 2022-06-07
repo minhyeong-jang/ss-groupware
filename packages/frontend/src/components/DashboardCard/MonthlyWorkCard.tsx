6a4a9a6585c3bffe03cf6fc702f4f6b5701a8f9f
Minhyeong Jang <public.doriri@gmail.com>
2022-06-07 14:10:57 +0900

    fix: 문구 변경

--- packages/frontend/src/components/DashboardCard/MonthlyWorkCard.tsx
+++ packages/frontend/src/components/DashboardCard/MonthlyWorkCard.tsx
@@ -8,17 +8,17 @@ interface Props {
 }
 export const MonthlyWorkCard: FC<Props> = ({ monthlyWork }) => {
   return (
-    <DashboardCard icon={DashboardCardIcon.RUN} title='월간 업무 내용'>
+    <DashboardCard icon={DashboardCardIcon.RUN} title='월간 근무 시간'>
       <ContentLabel>
-        근무시간 ( 법정 근로 시간 : {monthlyWork.officialHour}시간 )
+        근무시간 ( 법정 근무 시간 : {monthlyWork.officialHour}시간 )
       </ContentLabel>
       <ContentDesc>
         {monthlyWork.workHour}시간 {monthlyWork.workMinute}분
       </ContentDesc>
       <ContentNotice>
-        근로시간은 휴가, 출퇴근 누락이 포함되지 않아요.
+        근무시간은 휴가, 출퇴근 누락이 포함되지 않아요.
         <br />
-        법정근로시간 = 영업일 기준(휴가 제외)*8
+        법정근무시간 = 영업일 기준(휴가 제외)*8
       </ContentNotice>
     </DashboardCard>
   );
