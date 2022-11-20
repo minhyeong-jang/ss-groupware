3ca9a8cacd4f1e3bd3712682d4ab6d9c3be18dc7
Minhyeong Jang <public.doriri@gmail.com>
2022-11-20 20:06:04 +0900

    fix: build 파일 제거

--- packages/backend/build/types/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CompanyType = void 0;
-var CompanyType;
-(function (CompanyType) {
-    CompanyType["MUSINSA"] = "MUSINSA";
-    CompanyType["MUSINSALAB"] = "MUSINSALAB";
-})(CompanyType = exports.CompanyType || (exports.CompanyType = {}));
